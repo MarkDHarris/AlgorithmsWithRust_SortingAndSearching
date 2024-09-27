0c118ce2dd71bd9f4d503d36e3f6b2085e81a652
Mark Harris <mark.d.harris@outlook.com>
2024-09-27 12:11:37 -0400

    Adding completed cocktail shaker bubble sort

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -16,3 +16,42 @@ pub fn bubble_sort(vec: &mut Vec<i32>) {
         n = n - 1;
     }
 }
+
+pub fn cocktail_shaker_bubble_sort(vec: &mut Vec<i32>) {
+    let length = vec.len();
+    let mut swapped;
+    let mut start = 0;
+    let mut end = length - 1;
+
+    loop {
+        swapped = false;
+
+        // forward pass
+        for i in start..end {
+            if vec[i] > vec[i + 1] {
+                vec.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+
+        end -= 1;
+
+        if !swapped || start > end {
+            break;
+        }
+
+        swapped = false;
+
+        // backward pass
+        for i in (start..end).rev() {
+            if vec[i] > vec[i + 1] {
+                vec.swap(i, i + 1);
+                swapped = true;
+            }
+        }
+        start += 1;
+        if !swapped || start > end {
+            break;
+        }
+    }
+}
