4d65b14377caf2de3ff24a5a64b8a576bfe4b914
Mark Harris <mark.d.harris@outlook.com>
2025-06-12 10:45:49 -0400

    Adding partial fixes to dockerfile

---
+++ .devcontainer/resources/.bashrc
@@ -0,0 +1,4 @@
+# asdf
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+# starship 
+eval "$($HOME/.asdf/shims/starship init bash)"
--- .devcontainer/scripts/run.sh
+++ .devcontainer/scripts/run.sh
@@ -4,7 +4,15 @@ start_time=$(date +%s)
 USERNAME="maharris"
 IMAGENAME="rustdev:latest"
 
-docker run -it --rm --mount "type=bind,src=./,target=/home/$USERNAME/project" --entrypoint /bin/bash $IMAGENAME
+# docker run -it --rm --volume /var/run/docker.sock:/var/run/docker.sock \
+# --mount "type=bind,src=./,target=/home/$USERNAME/project" \
+# --entrypoint /bin/bash $IMAGENAME
+
+docker run -it --rm \
+    --privileged \
+    --name my-dind-container \
+    --mount "type=bind,src=./,target=/home/$USERNAME/project" \
+    $IMAGENAME
 
 end_time=$(date +%s)
 duration=$((end_time - start_time))
